808d41907944261f288666f7a8ffc4501d3889c5
nicolasArckman <nicolas.ribeiro@xcelis.com.br>
2026-03-30 11:41:00 -0300

    atualizando job

--- scheduler/jobs.py
+++ scheduler/jobs.py
@@ -5,7 +5,7 @@ def registrar_jobs(scheduler):
     scheduler.add_job(
         loger, # função main do app
         trigger="cron",
-        minutes="0,20,40",
+        minute="0,20,40",
         id="app_loger",
         #next_run_time=datetime.now() ,  # executa imediatamente ao subir
         name="App Loger - a cada 20 min",
